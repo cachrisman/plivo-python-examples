0d65b2b1c54ff9cc53cb31bf5863c5d56a2a4aeb
Charlie Chrisman <charlie.chrisman@gmail.com>
2015-09-25 15:33:39 -0700

    stupid smart quotes...

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def play_xml():
 @app.route("/recordxml/", methods=['GET'])
 def record_xml():
     """
-    Returns a <Record> element. This element records the caller’s
+    Returns a <Record> element. This element records the caller's
     voice and stores the file containing the audio recording.
     """
 
